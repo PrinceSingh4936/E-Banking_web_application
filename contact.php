908abe43ae698d2d1c2e8a27847017193244cbd2
PrinceSingh4936 <153479636+PrinceSingh4936@users.noreply.github.com>
2023-12-19 15:00:08 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,45 @@
+<?php
+    include "header.php";
+    include "navbar.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="contact_style.css">
+</head>
+
+<body>
+    <div class="flex-container-background">
+        <div class="flex-container-heading">
+            <h1 id="contact">Contact Us</h1>
+        </div>
+
+        
+
+        <div class="flex-container" style="border-top: 0;
+                                           border-bottom-left-radius: 10px;
+                                           border-bottom-right-radius: 10px;">
+            <div class="flex-item">
+                <h1 id="sub-contact">Customer Care (24x7)</h1>
+                <p id="sub-contact">
+                    Toll-Free: 888-966-6992<br>
+                    Phone: 732-666-5555<br>
+                    Email: care@bankpsit.com
+                </p>
+            </div>
+            <div class="flex-item">
+                <h1 id="sub-contact">Live Chat</h1>
+                <p id="sub-contact">
+                    Download our app and live chat<br>
+                    with our customer care !<br>
+                    App available on Google Play<br>
+                    and iPhone-AppStore.
+                </p>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
